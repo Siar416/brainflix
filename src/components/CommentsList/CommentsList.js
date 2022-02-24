3ed13dc9a36fe6030141bdfcf319c1b06205b254
Siar Wahidi <siar.wahidi@gmail.com>
2022-02-24 10:02:51 -0500

    Added the 3 comments and styled them

--- src/components/CommentsList/CommentsList.js
+++ src/components/CommentsList/CommentsList.js
@@ -1,22 +1,45 @@
 import comments from "../../data/video-details.json";
+import "./CommentsList.scss";
 
-// console.log(comments[0].comments[0].name);
+console.log(comments[0]);
 let data = comments[0].comments;
-console.log(data);
 
 function CommentsList() {
   return (
-    <section className="comments">
-      <img className="comments__avatar" alt="round circle" />
-
-      <section className="comments__wrapper">
-        <p className="comments__name">{data[0].name}</p>
-        <p className="comments__date">
-          {new Date(data[0].timestamp).toLocaleDateString()}
-        </p>
-        <p className="comments__comment">{data[0].comment}</p>
+    <>
+      <section className="comments__list">
+        <section className="comments__wrapper">
+          <p className="comments__wrapper__avatar" />
+          <p className="comments__wrapper__name">{data[0].name}</p>
+          <p className="comments__wrapper__date">
+            {new Date(data[0].timestamp).toLocaleDateString()}
+          </p>
+        </section>
+        <p className="comments__list__comment">{data[0].comment}</p>
       </section>
-    </section>
+
+      <section className="comments__list">
+        <section className="comments__wrapper">
+          <p className="comments__wrapper__avatar" />
+          <p className="comments__wrapper__name">{data[1].name}</p>
+          <p className="comments__wrapper__date">
+            {new Date(data[1].timestamp).toLocaleDateString()}
+          </p>
+        </section>
+        <p className="comments__list__comment">{data[1].comment}</p>
+      </section>
+
+      <section className="comments__list">
+        <section className="comments__wrapper">
+          <p className="comments__wrapper__avatar" />
+          <p className="comments__wrapper__name">{data[2].name}</p>
+          <p className="comments__wrapper__date">
+            {new Date(data[2].timestamp).toLocaleDateString()}
+          </p>
+        </section>
+        <p className="comments__list__comment">{data[2].comment}</p>
+      </section>
+    </>
   );
 }
 

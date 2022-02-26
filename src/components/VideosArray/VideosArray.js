66f68ab6530c7fe42dcf1c913fcc1029f6d58ec1
Siar Wahidi <siar.wahidi@gmail.com>
2022-02-25 23:03:30 -0500

    did some more styling. Also, trying to style the list of videos

--- src/components/VideosArray/VideosArray.js
+++ src/components/VideosArray/VideosArray.js
@@ -1,15 +1,19 @@
+import "./VideosArray.scss";
+
 function VideosArray(props) {
   console.log(props.data);
 
   return (
     <>
       {props.data.map((item) => (
-        <>
-          <h1>{item.title}</h1>
-          <h1>{item.channel}</h1>
-          <h1>{item.id}</h1>
-          {/* <img src={item.image} /> */}
-        </>
+        <section className="videos">
+          {<img className="videos__img" src={item.image} />}
+          <section className="videos__wrapper">
+            <h1 className="videos__title">{item.title}</h1>
+            <h1 className="videos__channel">{item.channel}</h1>
+            <h1 className="videos__id">{item.id}</h1>
+          </section>
+        </section>
       ))}
     </>
   );

2540d31156e76c8f85c68785240bfafd4b1043da
Siar Wahidi <siar.wahidi@gmail.com>
2022-03-11 22:30:30 -0500

    added functionality to form upload

--- src/pages/VideoUploadPage/VideoUploadPage.js
+++ src/pages/VideoUploadPage/VideoUploadPage.js
@@ -1,64 +1,92 @@
 import videoThumbnail from "../../assets/images/Upload-video-preview.jpg";
 import publishIcon from "../../assets/icons/publish.svg";
 import "./VideoUploadPage.scss";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { Component } from "react";
+import axios from "axios";
+const url = "http://localhost:8000/videos/";
+const { v4: uuidv4 } = require("uuid");
 
-function VideoUploadPage() {
-  const uploadBtn = (event) => {
-    event.preventDefault();
-    alert("Thank you! You're video has been uploaded!");
+class VideoUploadPage extends Component {
+  state = {
+    isSubmitted: false,
   };
 
-  return (
-    <section className="uploads">
-      <section className="uploads__wrapper">
-        <h1 className="uploads__title">Upload Video</h1>
-        <form className="form">
-          <section className="form__thumbnail">
-            <h2 className="form__thumbnail__title">VIDEO THUMBNAIL</h2>
-            <img
-              className="form__thumbnail__image"
-              src={videoThumbnail}
-              alt="image of person riding a bike"
-            />
-          </section>
-          <section className="form__section">
-            <label className="form__section__title">TITLE YOUR VIDEO</label>
-            <input
-              name="title"
-              type="text"
-              className="form__section__title-input"
-              placeholder="Add a title to your video"
-            />
-            <label className="form__section__desc">
-              ADD A VIDEO DESCRIPTION
-            </label>
-            <input
-              name="description"
-              type="text"
-              className="form__section__desc-input"
-              placeholder="Add a description to your video"
-            />
-          </section>
-        </form>
+  uploadBtn = (event) => {
+    event.preventDefault();
 
-        <div className="uploads__button">
-          <button className="uploads__button-publish" onClick={uploadBtn}>
-            <Link to="/">
-              <img
-                className="uploads__button-publish-icon"
-                src={publishIcon}
-                alt="publish icon"
-              />
-              PUBLISH
-            </Link>
-          </button>
+    const title = event.target.title.value;
+    const desc = event.target.description.value;
+    const id = uuidv4();
 
-          <button className="uploads__button-cancel">CANCEL</button>
-        </div>
+    if (title && desc) {
+      alert("Upload has uploaded successfully");
+      axios.post(url, { title, desc, id });
+
+      this.setState({
+        isSubmitted: true,
+      });
+    } else {
+      alert("Unable to load video, form not complete");
+    }
+  };
+
+  render() {
+    if (this.state.isSubmitted) {
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <section className="uploads">
+        <section className="uploads__wrapper">
+          <h1 className="uploads__title">Upload Video</h1>
+          <form className="form" onSubmit={this.uploadBtn}>
+            <section className="test-1">
+              <section className="form__thumbnail">
+                <h2 className="form__thumbnail__title">VIDEO THUMBNAIL</h2>
+                <img
+                  className="form__thumbnail__image"
+                  src={videoThumbnail}
+                  alt="image of person riding a bike"
+                />
+              </section>
+              <section className="form__section">
+                <label className="form__section__title">TITLE YOUR VIDEO</label>
+                <input
+                  name="title"
+                  type="text"
+                  className="form__section__title-input"
+                  placeholder="Add a title to your video"
+                />
+                <label className="form__section__desc">
+                  ADD A VIDEO DESCRIPTION
+                </label>
+                <input
+                  name="description"
+                  type="text"
+                  className="form__section__desc-input"
+                  placeholder="Add a description to your video"
+                />
+              </section>
+            </section>
+            <div className="test">
+              <div className="uploads__button">
+                <button className="uploads__button-publish">
+                  <img
+                    className="uploads__button-publish-icon"
+                    src={publishIcon}
+                    alt="publish icon"
+                  />
+                  PUBLISH
+                </button>
+
+                <button className="uploads__button-cancel">CANCEL</button>
+              </div>
+            </div>
+          </form>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
-
 export default VideoUploadPage;

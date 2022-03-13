b608f1c4aeca7e5e2e9f21d37bc0478f352e3f8a
Siar Wahidi <siar.wahidi@gmail.com>
2022-03-13 14:20:58 -0400

    added feature to scroll to top when video clicked

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,8 +2,16 @@ import "./Video.scss";
 import { Link } from "react-router-dom";
 
 function Video({ id, image, title, channel }) {
+  const clickHandler = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <li>
+    <li onClick={() => clickHandler()}>
       <Link to={`/videos/${id}`}>
         <section className="videos__item">
           <img className="videos__item__img" src={image} alt="poster" />
